601e0057d10161b2b937e523daa5828d201cc6bb
Marc <billpayingmarc@gmail.com>
2026-01-04 14:23:13 -0500

    Q1

---
+++ app.js
@@ -0,0 +1,15 @@
+function showRating(rating) {
+    let ratings = "";
+//     for (let i = 0; i < rating; ++i) {
+//         ratings = ratings + "*"
+//         if (i !== ratings - 1) {
+//             ratings = ratings + " "
+//         }
+//     }
+//     return ratings;
+for (let i = 0; i < 4.5; ++i) {
+    console.log(i);
+}
+}
+
+console.log(showRating(4.5))
