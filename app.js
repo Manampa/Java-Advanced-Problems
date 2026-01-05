676fef90ba85257b0f5e9da99cc757dddd2dc0fd
Marc <billpayingmarc@gmail.com>
2026-01-05 11:11:26 -0500

    Q2

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
-function showRating(rating) {
-    let ratings = "";
-//     for (let i = 0; i < rating; ++i) {
+
+// Q1 directions below----------------------------------
+
+// initialise and empty string
+
+// loop through the rounded down rating
+    //  add a star for every iteration
+    // if its not the last iteration. add a space
+    
+    // if the number is not a integer
+    // add a full stop
+    
+    // return it
+    
+// Q1 directions above----------------------------------
+
+
+// Q1 below
+// function showRating(rating) {
+//     let ratings = "";
+//     for (let i = 0; i < Math.floor(rating); ++i) {
 //         ratings = ratings + "*"
-//         if (i !== ratings - 1) {
+//         if (i !== Math.floor(ratings) - 1) {
 //             ratings = ratings + " "
 //         }
 //     }
+//     if (!Number.isInteger(rating)) {
+//         ratings = ratings + "."
+//     }
 //     return ratings;
-for (let i = 0; i < 4.5; ++i) {
-    console.log(i);
-}
+
+// }
+
+// console.log(showRating(0.5))
+
+// Q1 above
+
+// Q1 Cleaner code(+=) below
+
+function showRating(rating) {
+    let ratings = "";
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratings += "*"
+        if (i !== Math.floor(ratings) - 1) {
+            ratings += " "
+        }
+    }
+    if (!Number.isInteger(rating)) {
+        ratings += "."
+    }
+    return ratings;
+    
 }
 
-console.log(showRating(4.5))
+console.log(showRating(3.5))
+// Q1 Cleaner code(+=) above
+
